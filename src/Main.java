0611235f899a6eef7d606c1b7bc0299f1a2d0f04
VishnuSanal <t.v.s10123@gmail.com>
2022-01-03 22:43:42 +0530

    multiple vars

--- src/Main.java
+++ src/Main.java
@@ -9,30 +9,44 @@ public class Main {
 
             Scanner scanner = new Scanner(System.in);
 
-            System.out.println("PS: Pass command line args in the order VARIABLE_NAME DATA_TYPE to make things quick!");
-
-            System.out.print("Enter variable name: ");
-            varName = scanner.next().trim();
+            System.out.println("----------------------------------------------------------------------------------------------------");
+            System.out.println("-----You can pass command line args in the order VARIABLE_NAME DATA_TYPE to make things quick!------");
+            System.out.println("---------------------Try multiple variables at once by passing them alternately---------------------");
+            System.out.println("--------------------------For eg: java Main String name String value int i--------------------------");
+            System.out.println("----------------------------------------------------------------------------------------------------");
 
             System.out.print("Enter data type: ");
             dataType = scanner.next().trim();
 
+            System.out.print("Enter variable name: ");
+            varName = scanner.next().trim();
+
+            System.out.println(generateGetterAndSetter(varName, dataType));
+
         } else {
 
-            varName = args[0].trim();
-            dataType = args[1].trim();
+            for (int i = 0; i < args.length; i++) {
 
+                try {
+
+                    System.out.println(generateGetterAndSetter(args[i], args[++i]));
+
+                } catch (ArrayIndexOutOfBoundsException e) {
+                    System.out.println("An element missing! Skipping the last variable");
+                    break;
+                }
+
+            }
         }
+    }
 
+    private static String generateGetterAndSetter(String dataType, String varName) {
         String getter =
                 "\tpublic " + dataType + " get" + Character.toUpperCase(varName.charAt(0)) + varName.substring(1) +
                         "() {\n\t\treturn " + varName + ";\n\t}";
         String setter = "\tpublic void set" + Character.toUpperCase(varName.charAt(0)) + varName.substring(1) +
                 "() {\n\t\tthis." + varName + " = " + varName + ";\n\t}";
 
-        System.out.println(getter);
-        System.out.println();
-        System.out.println(setter);
-
+        return getter + "\n\n" + setter;
     }
 }
